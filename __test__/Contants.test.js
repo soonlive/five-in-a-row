4da35b3825c12c5e5d684cf47fb4318bf0fd6e60
soonlive <soon@live.cn>
2017-05-28 14:02:03 +0800

    chore: support unit testing

---
+++ __test__/Contants.test.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Xin on 28/05/2017.
+ */
+
+test('same row points size is 5 ', () => {
+  expect(Constants.SAME_ROW_POINTS_SIZE).toBe(5);
+});
---
+++ preprocessor.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Xin on 28/05/2017.
+ */
+const fs = require('fs');
+eval(fs.readFileSync('./js/Constants.js')+';global.Constants = Constants;');
+eval(fs.readFileSync('./js/EventHub.js')+';global.EventHub = EventHub;');
+eval(fs.readFileSync('./js/model/Point.js')+';global.Point = Point;');
+eval(fs.readFileSync('./js/model/Player.js')+';global.Player = Player;');
+eval(fs.readFileSync('./js/model/AIPlayer.js')+';global.AIPlayer = AIPlayer;');
+eval(fs.readFileSync('./js/model/Board.js')+';global.Board = Board;');
+eval(fs.readFileSync('./js/components/GoBoard.js')+';global.GoBoard = GoBoard;');
+eval(fs.readFileSync('./js/components/Stone.js')+';global.Stone = Stone;');
+eval(fs.readFileSync('./js/components/CircleStone.js')+';global.CircleStone = CircleStone;');
+eval(fs.readFileSync('./js/components/CrossStone.js')+';global.CrossStone = CrossStone;');
+eval(fs.readFileSync('./js/View.js')+';global.View = View;');
+eval(fs.readFileSync('./js/Controller.js')+';global.Controller = Controller;');
+eval(fs.readFileSync('./js/App.js')+';global.App = App;');
